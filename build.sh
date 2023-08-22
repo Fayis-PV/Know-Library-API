53affea3ea80ef46380dc62552081cb986857c98
fayis <fayispvchelari@gmail.com>
2023-08-22 22:19:45 +0530

    create super user

--- build.sh
+++ build.sh
@@ -5,4 +5,7 @@ python -m pip install --upgrade pip
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
+
+python manage.py makemigrations
 python manage.py migrate
+python manage.py create_superuser
